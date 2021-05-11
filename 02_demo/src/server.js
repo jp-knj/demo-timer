9e7f831cd1beb4809be064ddc654733af5a45531
jp-knj <kenji.tomita1996@gmail.com>
2021-05-12 07:38:11 +0900

    :sparkles: Add server
    t

---
+++ 02_demo/src/server.js
@@ -0,0 +1,16 @@
+const server = () => (`
+  <html>
+    <head>
+      <title>CountUp</title>
+      <meta charset="utf-8"/>
+    </head>
+    <body>
+      <div id="app">
+        <h1>0</h1>
+        <button type="button">+</button>
+        <p>${new Date().toTimeString()}</p>
+      </div>
+    </body>
+  </html>`
+);
+export default server;
